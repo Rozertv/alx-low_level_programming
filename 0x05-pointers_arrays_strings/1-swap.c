afff99b5a7b182a5366fde45ca3e2499c124b7cc
Rozertv <vivianrozert@gmail.com>
2023-06-27 07:39:16 -0700

    Battery

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - take in two variable integers and swap them
+ * @authorv Anisah
+ * @a: swaps and stores adresse of b
+ * @b: swaps and stores adress of a
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+int swap;
+change = *b;
+*b = *a;
+*a = change;
+}
