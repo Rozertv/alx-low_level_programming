9f1444efc84b42129979030b6cc39180a86a623a
Rozertv <Vivianrozert@gmail.com>
2023-07-08 05:02:36 -0700

    Important dream

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,4 @@
-#include &quot;main.h&quot;
+#include "main.h"
 /**
  * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
@@ -7,7 +7,9 @@ void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
+	
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
-	{
-}
+	}
+
+} 
